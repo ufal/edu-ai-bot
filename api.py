b76d87c2b07e9254cfe9b18df75b1d035af5815d
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2022-05-17 20:28:21 +0200

    Adding QA model

--- api.py
+++ api.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python3
 
+import os
 import flask
 from argparse import ArgumentParser
 from flask import request, jsonify
 from solr_query import ask_solr
 from logzero import logger
 
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+QA_MODEL_PATH = os.path.join(os.path.dirname(__file__), 'multilingual_qaqg')
+
+if os.path.isdir(QA_MODEL_PATH):
+    from multilingual_qaqg.mlpipelines import pipeline
+    qa_model = pipeline("multitask-qa-qg",
+                        os.path.join(QA_MODEL_PATH, "checkpoint-185000"),
+                        os.path.join(QA_MODEL_PATH, "mt5_qg_tokenizer"))
+else:
+    logger.warn('Could not find QA directory, will run without it')
+    qa_model = None
 
 
 @app.route('/', methods=['POST'])
 def ask():
     query = request.json['q']
     logger.info(f'Q: {query}')
-    answer = ask_solr(query=query)
+    db_result = ask_solr(query=query)
 
-    for doc in answer['docs']:
+    if not db_result.get('docs'):
+        logger.info(f'No result.')
+        return jsonify({'a': 'Toto bohužel nevím.'})
+
+    for doc in db_result['docs']:
         logger.info(f'D: {doc["title"]}')
-    return jsonify({'a': answer})
+    answer = db_result['docs'][0]
+    url = 'https://cs.wikipedia.org/wiki/' + answer["title"].replace(' ', '_')
+    if request.json.get('exact') and qa_model:
+        response = qa_model({'question': query, 'context': answer['first_paragraph']})
+        return jsonify({'a': f'Myslím, že {response} (Zdroj: {url})'})
+    return jsonify({'a': f'Tohle by vám mohlo pomoct: {answer["first_paragraph"]} (Zdroj: {url})'})
 
 
 if __name__ == '__main__':
