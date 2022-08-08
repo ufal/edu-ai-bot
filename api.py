7b9a0c443254ea420adf6058c864e3a1a96764c2
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2022-08-08 16:05:00 +0200

    A bit more sophisticated asking

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import os
 import flask
 from argparse import ArgumentParser
 from flask import request, jsonify
-from solr_query import ask_solr
+from solr_query import ask_solr, filter_query
 from logzero import logger
 
 app = flask.Flask(__name__)
@@ -25,21 +25,37 @@ else:
 @app.route('/', methods=['POST'])
 def ask():
     query = request.json['q']
-    logger.info(f'Q: {query}')
-    db_result = ask_solr(query=query)
+    filtered_query, query_type = filter_query(query)
+    logger.info(f'Q: {query} | F: {filtered_query}')
+    for q, a in [(f'"{filtered_query}"', 'title_str'),
+                 (f'"{filtered_query}"', 'title_cz'),
+                 (f'"{filtered_query}"', 'first_paragraph_cz'),
+                 (filtered_query, 'title_cz'),
+                 (filtered_query, 'first_paragraph_cz')]:
+        db_result = ask_solr(query=q, attrib=a)
+        if db_result.get('docs'):
+            break
 
     if not db_result.get('docs'):
         logger.info(f'No result.')
         return jsonify({'a': 'Toto bohužel nevím.'})
 
-    for doc in db_result['docs']:
-        logger.info(f'D: {doc["title"]}')
-    answer = db_result['docs'][0]
-    url = 'https://cs.wikipedia.org/wiki/' + answer["title"].replace(' ', '_')
-    if request.json.get('exact') and qa_model:
-        response = qa_model({'question': query, 'context': answer['first_paragraph']})
+    logger.debug("\n" + "\n".join([f'D: {doc["title"]}/{doc["score"]}' for doc in db_result['docs']]))
+
+    answers = db_result['docs']
+    url = 'https://cs.wikipedia.org/wiki/' + answers[0]["title"].replace(' ', '_')
+    if request.json.get('exact') and query_type == 'default' and qa_model:
+        # reranking by QA decoding score -- doesn't seem to work
+        #resp_cands = []
+        #for context in [a['first_paragraph'] for a in answers[:1]]:
+            #resp_cands.append(qa_model({'question': query, 'context': context}))
+        #logger.debug('RCs:\n' + "\n".join(['RC: %s | %f' % rc for rc in resp_cands]))
+        #response, _ = max(resp_cands, key=lambda rc: rc[1])
+        # feeding multiple contexts -- doesn't seem to work
+        context = "\n".join([a['first_paragraph'] for a in answers[:1]])
+        response, _ = qa_model({'question': query, 'context': context})
         return jsonify({'a': f'Myslím, že {response} (Zdroj: {url})'})
-    return jsonify({'a': f'Tohle by vám mohlo pomoct: {answer["first_paragraph"]} (Zdroj: {url})'})
+    return jsonify({'a': f'Tohle by vám mohlo pomoct: {answers[0]["first_paragraph"]} (Zdroj: {url})'})
 
 
 if __name__ == '__main__':
--- solr_query.py
+++ solr_query.py
@@ -3,18 +3,49 @@ import requests
 
 from logzero import logger
 from werkzeug.urls import url_fix
+from utils import dotdict
 
-URL_TO_SOLR = "http://quest.ms.mff.cuni.cz/namuddis/qasolr/wiki_test/query?q={query}&wt=json"
+URL_TO_SOLR = "http://quest.ms.mff.cuni.cz/namuddis/qasolr/wiki_test/query?q={query}&wt=json&fl=title,first_paragraph,url,score"
+URL_TO_UDPIPE = "http://lindat.mff.cuni.cz/services/udpipe/api/process?tokenizer&tagger&data={query}"
 
-def ask_solr(*, query):
+
+def ask_solr(*, query, attrib=None):
+    if attrib is not None:
+        if isinstance(attrib, list):
+            query = ' OR '.join([f'{a}:{query}' for a in attrib])
+        else:
+            query = f'{attrib}:{query}'
     response = requests.get(
         url_fix(URL_TO_SOLR.format(query=query))
     )
     j = json.loads(response.content.decode('utf8'))['response']
-    logger.info(j)
+    logger.debug(query + "\n" + str(j))
 
     return j
 
-    # response = json.load(connection.response)
-    # logger.info(response)
-    # return response
+
+STOP_WORDS = set([
+    'co', 'kdo', 'kdy', 'kde', 'jak', 'kolik', 'být', 'ten', '?', '.', ':', '!',
+    'znamenat', 'jaký', 'který', 'mít', 'proč',
+])
+
+
+def filter_query(query):
+    try:
+        udpipe = requests.get(url_fix(URL_TO_UDPIPE.format(query=query)))
+        tagged = [line.split("\t") for line in udpipe.json()['result'].split("\n")
+                  if "\t" in line and not line.startswith('#')]
+        tagged = [dotdict({'form': w[1], 'lemma': w[2], 'tag': w[4]}) for w in tagged]
+    except Exception as e:
+        logger.warn('UDpipe problem:' + str(e))
+        return query
+    logger.debug("\n" + "\n".join(["\t".join(w) for w in tagged]))
+    filtered = " ".join([w.form for w in tagged
+                         if w.tag[0] in set(['N', 'A', 'C']) and w.lemma not in STOP_WORDS])
+
+    qtype = 'default'
+    if tagged[0].tag[0] == 'V':
+        qtype = 'Y/N'
+    if tagged[0].lemma == 'proč':
+        qtype = 'why'
+    return filtered, qtype
