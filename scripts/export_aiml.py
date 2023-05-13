423437072e0af63a7d9d838f04a4fc0371d57091
Vojta <vojta.hudecek@gmail.com>
2023-05-13 10:39:35 +0200

    updated export_aiml.py

--- scripts/export_aiml.py
+++ scripts/export_aiml.py
@@ -8,7 +8,7 @@ from yaml.loader import SafeLoader
 from edubot.remote_services import RemoteServiceHandler
 
 
-def process_file(inp_file, remote_service_handler, ofd, sep="\t"):
+def process_file(inp_file, remote_service_handler, ofd, sep="\t", cached=None):
     print(inp_file)
     # extract all the contents from the <category> tags using xml parser
     # extract pairs from the <pattern> and <template> tags
@@ -19,33 +19,44 @@ def process_file(inp_file, remote_service_handler, ofd, sep="\t"):
     for child in root:
         if child.tag == "category":
             pattern = child.find("pattern").text
+            if pattern is not None:
+                pattern = pattern.strip().lower()
+            else:
             template = child.find("template").text
-            if template is None or pattern is None:
+            if template is not None:
+                templates = [template.strip().lower()]
+            else:
+                templates = []
+                for inner in child.find("template"):
+                    if inner.text is not None:
+                        templates.append(inner.text.strip().lower())
+                    for item in inner:
+                        txt = item.text
+                        if txt is not None:
+                            templates.append(txt.strip().lower())
+            if cached is not None and pattern in cached:
                 continue
-            try:
-                print(sep.join([pattern.lower().strip(),
-                                template.lower().strip(),
-                                remote_service_handler.translate_en2cs(pattern.lower().strip()).strip(),
-                                remote_service_handler.translate_en2cs(template.lower().strip())]).strip(),
-                      file=ofd, flush=True)
-            except Exception as e:
-                print(e)
+            if len(templates) == 0 or pattern is None:
                 continue
+            for tmpl in templates:
+                try:
+                    print(sep.join([pattern.lower().strip(),
+                                    tmpl.lower().strip(),
+                                    remote_service_handler.translate_en2cs(pattern.lower().strip()).strip(),
+                                    remote_service_handler.translate_en2cs(tmpl.lower().strip())]).strip(),
+                          file=ofd, flush=True)
+                except Exception as e:
+                    print(e)
+                    continue
 
 
 def main(args):
-    processed_files = ["alice.aiml", "update1.aiml", "inquiry.aiml", "history.aiml", "psychology.aiml", "mp4.aiml",
-                       "astrology.aiml", "iu.aiml", "computers.aiml", "client.aiml", "reduction1.safe.aiml",
-                       "religion.aiml", "drugs.aiml", "primeminister.aiml", "reductions-update.aiml",
-                       "music.aiml", "numbers.aiml", "personality.aiml", "geography.aiml", "food.aiml",
-                       "phone.aiml", "bot_profile.aiml", "ai.aiml", "reduction0.safe.aiml", "reduction2.safe.aiml",
-                       "stories.aiml", "movies.aiml", "biography.aiml", "science.aiml", "salutations.aiml",
-                       "reduction4.safe.aiml", "client_profile.aiml", "continuation.aiml", "money.aiml",
-                       "emotion.aiml", "mp1.aiml", "knowledge.aiml", "mp0.aiml", "mp2.aiml", "default.aiml",
-                       "sports.aiml","atomic.aiml", "reduction3.safe.aiml", "mp3.aiml", "mp6.aiml", "stack.aiml",
-                       "bot.aiml", "date.aiml", "sex.aiml", "xfind.aiml", "psychology.aiml", "loebner10.aiml",
-                       "reduction.names.aiml", "mp5.aiml"]
-    processed_files.extend([f"{args.input}/{f}" for f in processed_files])
+    processed = set()
+    with open(args.processed, 'rt') as fd:
+        for line in fd:
+            line = line.split("\t")
+            processed.add(line[0])
+
     if os.path.isdir(args.input):
         files = glob.glob(os.path.join(args.input, "*.aiml"))
     else:
@@ -55,9 +66,7 @@ def main(args):
     remote_service_handler = RemoteServiceHandler(config)
     with open(args.output, 'at') as ofd:
         for inp_file in files:
-            if inp_file in processed_files:
-                continue
-            process_file(inp_file, remote_service_handler, ofd, sep=args.separator)
+            process_file(inp_file, remote_service_handler, ofd, sep=args.separator, cached=processed)
             print("Done with file: ", inp_file)
 
 
@@ -65,6 +74,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, help="Path to input file/directory")
     parser.add_argument("--config", type=str, help="Path to config file")
+    parser.add_argument("--processed", type=str, help="Path to output file")
     parser.add_argument("--output", type=str, help="Path to output file")
     parser.add_argument("--separator", type=str, default="\t")
     args = parser.parse_args()
