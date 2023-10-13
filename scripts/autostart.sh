eab5415b66d0de897ef47a5b6ef6131dfc308475
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2023-10-13 16:18:46 +0200

    Adding email notif

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 USER=`whoami`
+EMAIL="odusek@ufal.mff.cuni.cz"
 PORT=8202
 API_KEY=$1
+SOLR_URL="http://quest.ms.mff.cuni.cz/namuddis/qasolr/wiki_test/query"
 
 date
+
+if curl "$SOLR_URL" 2>/dev/null; then
+    echo '.'
+else
+    echo 'Solr not running, sending alert...'
+    echo -e "Subject: EDU-AI Solr not running\nCheck the server" | sendmail "$EMAIL"
+fi
+
 if curl "http://localhost:$PORT/ping" 2>/dev/null; then
     echo '.'
 else
     echo 'Restarting...'
+    echo -e "Subject: Restarting EDU-AI bot\nYou may want to check the server" | sendmail "$EMAIL"
     cd /home/$USER/edu-ai-bot
     nohup bash -lc "conda activate edubot; OPENAI_API_KEY=$API_KEY python api.py --port $PORT --config configs/default_config.yaml --logfile log.jsonl 2>&1 | tee -a console-log.txt" &
 fi
