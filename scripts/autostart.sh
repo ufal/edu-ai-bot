5f504c192c8148cf635aa3f5f4c2061bf552fa50
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2023-11-09 16:35:59 +0100

    Fixing sendmail

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -12,14 +12,14 @@ if curl "$SOLR_URL" 2>/dev/null; then
     echo '.'
 else
     echo 'Solr not running, sending alert...'
-    echo -e "Subject: EDU-AI Solr not running\nCheck the server" | sendmail "$EMAIL"
+    echo -e "Subject: EDU-AI Solr not running\nCheck the server" | /usr/sbin/sendmail "$EMAIL"
 fi
 
 if curl "http://localhost:$PORT/ping" 2>/dev/null; then
     echo '.'
 else
     echo 'Restarting...'
-    echo -e "Subject: Restarting EDU-AI bot\nYou may want to check the server" | sendmail "$EMAIL"
+    echo -e "Subject: Restarting EDU-AI bot\nYou may want to check the server" | /usr/sbin/sendmail "$EMAIL"
     cd /home/$USER/edu-ai-bot
     nohup bash -lc "conda activate edubot; OPENAI_API_KEY=$API_KEY python api.py --port $PORT --config configs/default_config.yaml --logfile log.jsonl 2>&1 | tee -a console-log.txt" &
 fi
